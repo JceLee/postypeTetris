1e14f8a81e3672dff6d2b99e96eb826d803475c4
JceLee <lkm4351@gmail.com>
2021-03-11 11:58:01 +0900

    add comments

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -3,6 +3,7 @@ export default class ScoreBoard {
     document.getElementById("scoreBoard").style.display = "block";
   }
 
+  //점수판을 업데이트 합니다.
   updateBoard(state) {
     const { score, level } = state;
     document.getElementById("score").textContent = score;
